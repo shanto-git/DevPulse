0c20a30676388743acece4e3658950c3eeacb85a
Masuduzzaman <md.santo0583210@gmail.com>
2026-06-01 03:52:48 +0600

    update deletecontroller and create middleware

--- src/module/issues/issue.controller.ts
+++ src/module/issues/issue.controller.ts
@@ -98,16 +98,16 @@ const deleteIssue = async(req:Request,res:Response,)=>{
     const result = await issueService.deleteIssueFromDB(id as string)
 
     if(!result){
-      res.status(400).json({
+      return res.status(400).json({
         success:false,
         message:"Issue Not Found"
-      })
-      res.status(200).json({
+      })}
+      return res.status(200).json({
         success:true,
         message:"Issue deleted successfully",
         data:{}
-      })
-    }
+      });
+    
   } catch (error:any) {
     res.status(500).json({
       success: false,
